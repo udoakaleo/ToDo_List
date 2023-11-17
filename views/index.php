7fb927f5fec26ac08ae142085a8c2e1c8a6f453a
leo <leoudoaka@mail.com>
2023-11-17 19:20:53 +0100

    third commit

--- views/index.php
+++ views/index.php
@@ -9,23 +9,27 @@
 <body>
 <div class = 'container bg-light'>
 
-<div class = 'row bg-muted border: 1'>
-      <div class = 'col text-center m-4'>
-           <h3 class = ''>ToDo_List Application Using PHP and MYSQL database</h3>
-      </div>
+        <div class = 'row bg-muted border: 1'>
+          <div class = 'col text-center m-4'>
+              <h3 class = ''>ToDo_List Application Using PHP and MYSQL database</h3>
+           </div>
        
-   </div>
-<form method = 'post' action = index.php class = 'form-control'>
+        </div>
+        <form method = 'post' action = index.php class = 'form-control'>
    
-    <div class = 'row'>
-         <div class = 'col-9'>
-           <input type = 'text' name = 'todo' class = 'form-control bg-light'/>
-          </div>
-          <div class = 'col-3'>
-           <input type = 'submit' name = 'addTask' class = 'form-control bg-success text-white' value = 'AddTask'/>
-          </div>
-    </div>  
-</form>
+              <div class = 'row'>
+                  <div class = 'col-9'>
+                    <input type = 'text' name = 'todo' class = 'form-control bg-light'/>
+                     </div>
+                   <div class = 'col-3'>
+                  <input type = 'submit' name = 'addTask' class = 'form-control bg-success text-white' value = 'AddTask'/>
+                  </div>
+             </div>  
+           </form>
+
+            <table>
+
+            </table>
 </div>
 </body>
 </html>
