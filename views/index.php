dd9b824f9416ae4a935a019580e1b5647a8ca7c8
leo <leoudoaka@mail.com>
2023-11-17 14:51:05 +0100

    todo_list index page

--- views/index.php
+++ views/index.php
@@ -7,25 +7,25 @@
     <title>ToDo_List</title>
 </head>
 <body>
-     <div class = 'container bg-light'>
+<div class = 'container bg-light'>
 
-         <div class = 'row bg-muted border: 1'>
-               <div class = 'col text-center m-4'>
-                    <h3 class = ''>ToDo_List Application Using PHP and MYSQL database</h3>
-               </div>
-                
-            </div>
-         <form method = 'post' action = index.php class = 'form-control'>
-            
-             <div class = 'row'>
-                  <div class = 'col-9'>
-                    <input type = 'text' name = 'todo' class = 'form-control bg-light'/>
-                   </div>
-                   <div class = 'col-3'>
-                    <input type = 'submit' name = 'addTask' class = 'form-control bg-success text-white' value = 'AddTask'/>
-                   </div>
-             </div>  
-         </form>
-     </div>
+<div class = 'row bg-muted border: 1'>
+      <div class = 'col text-center m-4'>
+           <h3 class = ''>ToDo_List Application Using PHP and MYSQL database</h3>
+      </div>
+       
+   </div>
+<form method = 'post' action = index.php class = 'form-control'>
+   
+    <div class = 'row'>
+         <div class = 'col-9'>
+           <input type = 'text' name = 'todo' class = 'form-control bg-light'/>
+          </div>
+          <div class = 'col-3'>
+           <input type = 'submit' name = 'addTask' class = 'form-control bg-success text-white' value = 'AddTask'/>
+          </div>
+    </div>  
+</form>
+</div>
 </body>
 </html>
